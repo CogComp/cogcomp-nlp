ffc60ff777b9a777b2e199d60489e65124abc7f4
Subhro  Roy <subhroroy@Subhros-MacBook-Pro.local>
2014-12-16 20:54:30 -0600

    C

---
+++ src/main/java/edu/illinois/cs/cogcomp/quant/lbj/Constants.java
@@ -0,0 +1,17 @@
+package edu.illinois.cs.cogcomp.quant.lbj;
+
+
+/**
+  * Constant values used by the LBJ source file.
+  *
+  * @author Nick Rizzolo
+ **/
+public class Constants
+{ 
+  /** The path to a file containing training data. */
+  public static final String trainingData =
+    "data/trainingData.txt";
+    //"../../data/NE/Reuters2003/BIO.train";
+   //	"/shared/grandpa/corpora/chunking/conll2000distributions/train.txt";
+}
+
