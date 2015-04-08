249e076b3efed2d2ae5142ce2b50567c82f36dc5
Naveen Arivazhagan <arivazh2@illinois.edu>
2015-04-07 21:51:20 -0500

    forgot to add seeded shuffle update in CommaReader to previous commit removed unneccary imports from ClassifierComaprison.java
    Former-commit-id: e1c1b6dc756b65bc0f06872ebc806a931b5651c2

--- src/main/java/edu/illinois/cs/cogcomp/comma/ClassifierComparison.java
+++ src/main/java/edu/illinois/cs/cogcomp/comma/ClassifierComparison.java
@@ -9,16 +9,12 @@ import edu.illinois.cs.cogcomp.comma.lbj.LocalCommaClassifier;
 import edu.illinois.cs.cogcomp.comma.lbj.LocativePairConstrainedCommaClassifier;
 import edu.illinois.cs.cogcomp.comma.lbj.OxfordCommaConstrainedCommaClassifier;
 import edu.illinois.cs.cogcomp.comma.lbj.SubstitutePairConstrainedCommaClassifier;
-import edu.illinois.cs.cogcomp.comma.lbj.oxfordComma;
-import edu.illinois.cs.cogcomp.comma.lbj.substitutePair;
 import edu.illinois.cs.cogcomp.lbjava.classify.Classifier;
 import edu.illinois.cs.cogcomp.lbjava.classify.TestDiscrete;
-import edu.illinois.cs.cogcomp.lbjava.infer.ParameterizedConstraint;
 import edu.illinois.cs.cogcomp.lbjava.parse.Parser;
 
-
 public class ClassifierComparison {
-	public static void main(String[] args){
+	public static void main(String[] args) {
 		Classifier oracle = new CommaLabel();
 		List<Classifier> classifiers = new ArrayList<Classifier>();
 		classifiers.add(new LocalCommaClassifier());
@@ -26,14 +22,14 @@ public class ClassifierComparison {
 		classifiers.add(new LocativePairConstrainedCommaClassifier());
 		classifiers.add(new ListCommasConstrainedCommaClassifier());
 		classifiers.add(new OxfordCommaConstrainedCommaClassifier());
-		
+
 		Parser parser = new CommaReader("data/test_commas.txt", "data/test_commas.ser");
 
-		for(Classifier classifier : classifiers){
+		 for(Classifier classifier : classifiers){
 			System.out.println(classifier.name);
-			TestDiscrete.testDiscrete(classifier, oracle, parser).printPerformance(System.out);
-			parser.reset();
-			System.out.println();
-		}
+		 	TestDiscrete.testDiscrete(classifier, oracle, parser).printPerformance(System.out);
+		 	parser.reset();
+		 	System.out.println(); 
+		 }
 	}
 }
--- src/main/java/edu/illinois/cs/cogcomp/comma/CommaReader.java
+++ src/main/java/edu/illinois/cs/cogcomp/comma/CommaReader.java
@@ -14,6 +14,7 @@ import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Random;
 import java.util.Scanner;
 import java.util.Set;
 import java.util.regex.Matcher;
@@ -289,7 +290,9 @@ public class CommaReader implements Parser {
     
     public static void divideTDT(Collection<Sentence> sentences, String textSource) throws IOException{
     	List<Sentence> sentenceList = new ArrayList<Sentence>(sentences);
-    	Collections.shuffle(sentenceList);
+    	
+    	long seed = 4901945429246723620L; 
+    	Collections.shuffle(sentenceList, new Random(seed));
     	
     	
     	double train = 0.7;
