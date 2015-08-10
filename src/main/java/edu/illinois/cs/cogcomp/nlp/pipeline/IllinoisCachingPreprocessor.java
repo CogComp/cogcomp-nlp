32d0d79e99076bcd84113d54fe23deda6dc6df9b
Mark Sammons <mssammon@illinois.edu>
2015-08-10 16:44:31 -0500

    fixed initialization bug in IllinoisCachingPreprocessor

--- src/main/java/edu/illinois/cs/cogcomp/nlp/pipeline/IllinoisCachingPreprocessor.java
+++ src/main/java/edu/illinois/cs/cogcomp/nlp/pipeline/IllinoisCachingPreprocessor.java
@@ -14,6 +14,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 
 /**
@@ -150,6 +151,8 @@ public class IllinoisCachingPreprocessor  extends CachingAnnotatorService
      */
     @Override
     protected void setSupportedViews() {
+
+        supportedViews = new HashSet< String >();
         supportedViews.addAll( activeViews.keySet() );
     }
 
