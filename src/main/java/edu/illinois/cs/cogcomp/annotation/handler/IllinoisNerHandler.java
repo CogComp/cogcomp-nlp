148f5019cab5944d933d86652a99295683513b97
Mark Sammons <mssammon@illinois.edu>
2015-09-19 18:11:29 -0500

    added config options for Stanford Parser to limit processing time and memory size (indirectly through sentence length and a time limit) updated pipeline factory and IllinoisNerHandler to accommodate new NER constructor

--- src/main/java/edu/illinois/cs/cogcomp/annotation/handler/IllinoisNerHandler.java
+++ src/main/java/edu/illinois/cs/cogcomp/annotation/handler/IllinoisNerHandler.java
@@ -19,14 +19,23 @@ public class IllinoisNerHandler extends PipelineAnnotator
     private static final String VERSION = "2.8.5-SNAPSHOT";
     public NERAnnotator nerAnnotator;
 
-    public IllinoisNerHandler(String config, String modelType) throws IOException {
-        super( FULL_NAME_PREFIX + " " + modelType, VERSION, SHORT_NAME_PREFIX + " " + modelType );
-        this.nerAnnotator =  NerAnnotatorManager.buildNerAnnotator( config );
+    /**
+     *
+     * @param config
+     * @param viewName  the canonical name for this view.  Should usually be either ViewNames.NER_CONLL
+     *                  or ViewNames.NER_ONTONOTES, but if you create a different NER model, you must
+     *                  also create a new canonical name, extend the ViewNames class, and add the new
+     *                  name as a constant there.
+     * @throws IOException
+     */
+    public IllinoisNerHandler(String config, String viewName) throws IOException {
+        super( FULL_NAME_PREFIX + " " + viewName, VERSION, SHORT_NAME_PREFIX + " " + viewName );
+        this.nerAnnotator =  NerAnnotatorManager.buildNerAnnotator( config, viewName );
     }
 
     @Override
     public String getViewName() {
-        return ViewNames.NER_CONLL;
+        return this.nerAnnotator.getViewName();
     }
 
     @Override
