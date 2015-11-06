a9c9d1891d8d4c7d24d45453c45611f808b8bfe1
Nitish Gupta <gnnitish@gmail.com>
2015-11-05 20:18:40 -0600

    added function to get tf with lucene integer doc id.

--- src/main/java/edu/illinois/cs/cogcomp/bigdata/lucene/Lucene.java
+++ src/main/java/edu/illinois/cs/cogcomp/bigdata/lucene/Lucene.java
@@ -404,6 +404,34 @@ public class Lucene {
 		}
 	}
 
+	/**
+	 * returns tf-idf = (term_freq/inversve_doc_freq) for a given doc and a term.
+	 *
+	 * @param reader
+	 * @param docIdField
+	 * @param docId
+	 * @param textField
+	 * @param term
+	 * @return
+	 * @throws IOException
+	 */
+	public static double getTf(IndexReader reader, String docIdField, String docId, String textField, String term) throws IOException {
+		IndexSearcher searcher = new IndexSearcher(reader);
+		QueryParser parser = new QueryParser(version, docIdField, KEYWORD);
+		Query q = new TermQuery(new Term(docIdField, docId));
+
+		ScoreDoc[] docs = searcher.search(q, 1).scoreDocs;
+		if (docs.length == 0) {
+			System.err.println("Document with docId : " + docId + "  not found!");
+			return 0.0;
+		} else {
+			int luceneDocId = docs[0].doc;  // Lucene DocId
+			double tf = getTf(reader, luceneDocId, textField, term);
+			return tf;
+		}
+	}
+
+
 
 	/**
 	 * returns tf for a given term in a given doc
