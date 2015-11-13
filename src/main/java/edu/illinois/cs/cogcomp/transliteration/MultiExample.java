ef3fd86986d037caa861bdb48b08ee04b54a83d5
Stephen Mayhew <mayhew2@illinois.edu>
2015-11-13 10:18:51 -0600

    Added MultiExample, adding NEWS capability, about to make Example a subclass of MultiExample

---
+++ src/main/java/edu/illinois/cs/cogcomp/transliteration/MultiExample.java
@@ -0,0 +1,15 @@
+package edu.illinois.cs.cogcomp.transliteration;
+
+import java.util.List;
+
+/**
+ * This is meant to fulfill the same role as the Example class, but allows multiple targetwords to be associated with a single source word.
+ * Created by mayhew2 on 11/13/15.
+ */
+public class MultiExample {
+
+    public MultiExample(String sourceword, List<String> targetwords){
+
+    }
+
+}
