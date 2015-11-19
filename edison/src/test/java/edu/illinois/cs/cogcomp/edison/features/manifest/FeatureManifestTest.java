0be93ec651ea82e264728c7f03223424ff4cbfd1
Christos Christodoulopoulos <christod@illinois.edu>
2015-11-18 18:31:54 -0600

    Removed jwnl config from FeatureManifest Created unit test

---
+++ edison/src/test/java/edu/illinois/cs/cogcomp/edison/features/manifest/FeatureManifestTest.java
@@ -0,0 +1,42 @@
+package edu.illinois.cs.cogcomp.edison.features.manifest;
+
+import edu.illinois.cs.cogcomp.core.datastructures.ViewNames;
+import edu.illinois.cs.cogcomp.core.datastructures.textannotation.Constituent;
+import edu.illinois.cs.cogcomp.core.datastructures.textannotation.TextAnnotation;
+import edu.illinois.cs.cogcomp.core.io.IOUtils;
+import edu.illinois.cs.cogcomp.edison.features.Feature;
+import edu.illinois.cs.cogcomp.edison.features.FeatureExtractor;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+
+public class FeatureManifestTest {
+    public static final String file = "test.fex";
+    private static List<TextAnnotation> tas;
+
+    @Before
+    public void setUp() throws Exception {
+        tas = IOUtils.readObjectAsResource(FeatureManifestTest.class, "test.ta");
+    }
+
+    @Test
+    public void testCreateFex() throws Exception {
+        FeatureManifest featureManifest = new FeatureManifest(file);
+
+        featureManifest.useCompressedName();
+        featureManifest.setVariable("*default-parser*", ViewNames.PARSE_STANFORD);
+
+        FeatureExtractor fex = featureManifest.createFex();
+        Constituent c = tas.get(0).getView(ViewNames.TOKENS).getConstituents().get(0);
+        assertEquals("My", c.getSurfaceForm());
+        Set<Feature> features = fex.getFeatures(c);
+        Iterator<Feature> iterator = features.iterator();
+        Feature feature = iterator.next();
+        assertEquals("f:#ctxt#:context1::#wd:mother-in-law", feature.getName());
+    }
+}
