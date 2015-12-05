4e5faa9c2cde28defa263f813f58146b68e68bef
khashab2 <khashab2@illinois.edu>
2015-12-04 21:59:27 -0600

    small fix to the deploy script

--- deploy-artifacts.sh
+++ deploy-artifacts.sh
@@ -6,10 +6,10 @@ function deploy(){
 
 function setPermissions() {
   echo "We are about the set the permissions on bilbo ... "
-  ssh bilbo
+  ssh bilbo.cs.illinois.edu
   cd /mounts/bilbo/disks/0/www/cogcomp/html/m2repo/edu/illinois/cs/cogcomp
 
-  declare -a modules=("illinois-cogcomp-nlp" "illinois-core-utilities" "illinois-curator" "illinois-edison" "illinois-lemmatizer" "illinois-tokenizer")
+  declare -a modules=("illinois-cogcomp-nlp" "illinois-core-utilITIES" "illinois-curator" "illinois-edison" "illinois-lemmatizer" "illinois-tokenizer")
 
   for i in "${modules[@]}"
   do
