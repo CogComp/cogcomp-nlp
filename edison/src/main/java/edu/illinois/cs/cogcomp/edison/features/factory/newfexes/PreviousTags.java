f003b2cf22f7a8ddbfd052113e1b4fee4a56d1c7
- Paul Jones Vijayakumar <pvijaya2@fa15-cs241-031.cs.illinois.edu>
2015-12-13 22:08:16 -0600

    updated package for newfexes

--- edison/src/main/java/edu/illinois/cs/cogcomp/edison/features/factory/newfexes/PreviousTags.java
+++ edison/src/main/java/edu/illinois/cs/cogcomp/edison/features/factory/newfexes/PreviousTags.java
@@ -1,4 +1,4 @@
-package edu.illinois.cs.cogcomp.edison.features.factory;
+package edu.illinois.cs.cogcomp.edison.features.factory.newfexes;
 
 import edu.illinois.cs.cogcomp.core.datastructures.ViewNames;
 import edu.illinois.cs.cogcomp.core.datastructures.textannotation.View;
