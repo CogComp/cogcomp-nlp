a9d407cbcf538921149b81ac29969fdc27b65228
Mark Sammons <mssammon@illinois.edu>
2016-01-05 08:19:54 -0600

    added jwnl-prime dependency updated lemmatizer to have consistent configuration with rest of cogcomp-nlp updated lemmatizer tests to use new configuration, so they pass

---
+++ lemmatizer/src/main/java/edu/illinois/cs/cogcomp/nlp/lemmatizer/LemmatizerConfigurator.java
@@ -0,0 +1,26 @@
+package edu.illinois.cs.cogcomp.nlp.lemmatizer;
+
+import edu.illinois.cs.cogcomp.core.utilities.configuration.Configurator;
+import edu.illinois.cs.cogcomp.core.utilities.configuration.Property;
+import edu.illinois.cs.cogcomp.core.utilities.configuration.ResourceManager;
+
+/**
+ * Lemmatizer constructor parameters
+ * Created by mssammon on 1/5/16.
+ */
+public class LemmatizerConfigurator extends Configurator {
+
+    public final static Property WN_PATH = new Property( "wnPath", "wordnet/dict" );
+    public final static Property USE_STNFRD_CONVENTIONS = new Property( "useStanfordConventions", FALSE );
+
+    /**
+     * get a ResourceManager object with the default key/value pairs for this configurator
+     *
+     * @return a non-null ResourceManager with appropriate values set.
+     */
+    @Override
+    public ResourceManager getDefaultConfig() {
+        Property[] props = { WN_PATH, USE_STNFRD_CONVENTIONS };
+        return new ResourceManager( generateProperties( props ) );
+    }
+}
