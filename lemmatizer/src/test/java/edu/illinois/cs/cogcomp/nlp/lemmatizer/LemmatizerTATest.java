3c07ceaf8960d7fa8b8b5183d0b529bfbcf25829
Mark Sammons <mssammon@illinois.edu>
2016-01-06 16:58:29 -0600

    added dependency to non-serialized-wordnet jar updated lemmatizer to have consistent configuration with rest of cogcomp-nlp updated lemmatizer tests to use new configuration renamed lemmatizer tests so they are detected and run by maven

--- lemmatizer/src/test/java/edu/illinois/cs/cogcomp/nlp/lemmatizer/LemmatizerTATest.java
+++ lemmatizer/src/test/java/edu/illinois/cs/cogcomp/nlp/lemmatizer/LemmatizerTATest.java
@@ -20,7 +20,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 
-public class LemmatizerTestTA {
+public class LemmatizerTATest {
     private static final String TEST_TEXT_ANNOTATION_FILE = "src/test/resources/serializedTA.ser";
     private TextAnnotation inputTa;
     private IllinoisLemmatizer lem;
