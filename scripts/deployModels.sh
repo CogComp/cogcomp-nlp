c07c1a1303edf4d7d7b2962d70f590459f508b56
Christos Christodoulopoulos <christod@illinois.edu>
2016-01-31 20:22:11 -0600

    Updated models usage Now the models need to agree up to the minor version of the SRL releases (patch number shouldn't matter) The models now have their own Maven `artifactId` to be easily deployable.

--- scripts/deployModels.sh
+++ scripts/deployModels.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-VERSION=`mvn org.apache.maven.plugins:maven-help-plugin:2.1.1:evaluate -Dexpression=project.version | grep -v 'INFO'`
+## Important - change this ONLY when the minor version changes
+VERSION=5.1
 
 tmpdir=tmp-Srl-verb-$RANDOM
 
@@ -51,9 +52,9 @@ echo "Deploying illinois-srl-models-nom-CHARNIAK-${VERSION}.jar"
 mvn deploy:deploy-file \
     -Dfile=target/illinois-srl-models-nom-CHARNIAK-${VERSION}.jar \
     -DgroupId=edu.illinois.cs.cogcomp \
-    -DartifactId=illinois-srl \
+    -DartifactId=illinois-srl-models \
     -Dversion=${VERSION} \
-    -Dclassifier=models-nom-charniak \
+    -Dclassifier=nom-charniak \
     -Dpackaging=jar \
     -Durl=scp://bilbo.cs.illinois.edu:/mounts/bilbo/disks/0/www/cogcomp/html/m2repo \
     -DrepositoryId=CogcompSoftware
@@ -64,9 +65,9 @@ echo "Deploying illinois-srl-models-nom-STANFORD-${VERSION}.jar"
 mvn deploy:deploy-file \
     -Dfile=target/illinois-srl-models-nom-STANFORD-${VERSION}.jar \
     -DgroupId=edu.illinois.cs.cogcomp \
-    -DartifactId=illinois-srl \
+    -DartifactId=illinois-srl-models \
     -Dversion=${VERSION} \
-    -Dclassifier=models-nom-stanford \
+    -Dclassifier=nom-stanford \
     -Dpackaging=jar \
     -Durl=scp://bilbo.cs.illinois.edu:/mounts/bilbo/disks/0/www/cogcomp/html/m2repo \
     -DrepositoryId=CogcompSoftware
@@ -77,9 +78,9 @@ echo "Deploying illinois-srl-models-verb-CHARNIAK-${VERSION}.jar"
 mvn deploy:deploy-file \
     -Dfile=target/illinois-srl-models-verb-CHARNIAK-${VERSION}.jar \
     -DgroupId=edu.illinois.cs.cogcomp \
-    -DartifactId=illinois-srl \
+    -DartifactId=illinois-srl-models \
     -Dversion=${VERSION} \
-    -Dclassifier=models-verb-charniak \
+    -Dclassifier=verb-charniak \
     -Dpackaging=jar \
     -Durl=scp://bilbo.cs.illinois.edu:/mounts/bilbo/disks/0/www/cogcomp/html/m2repo \
     -DrepositoryId=CogcompSoftware
@@ -90,9 +91,9 @@ echo "Deploying illinois-srl-models-verb-STANFORD-${VERSION}.jar"
 mvn deploy:deploy-file \
     -Dfile=target/illinois-srl-models-verb-STANFORD-${VERSION}.jar \
     -DgroupId=edu.illinois.cs.cogcomp \
-    -DartifactId=illinois-srl \
+    -DartifactId=illinois-srl-models \
     -Dversion=${VERSION} \
-    -Dclassifier=models-verb-stanford \
+    -Dclassifier=verb-stanford \
     -Dpackaging=jar \
     -Durl=scp://bilbo.cs.illinois.edu:/mounts/bilbo/disks/0/www/cogcomp/html/m2repo \
     -DrepositoryId=CogcompSoftware
