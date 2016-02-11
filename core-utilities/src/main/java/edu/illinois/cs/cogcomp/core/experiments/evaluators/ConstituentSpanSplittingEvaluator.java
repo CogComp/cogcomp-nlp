493a2e3c1fb8b1f677d09f3032eab64db66082c8
khashab2 <khashab2@illinois.edu>
2016-02-11 03:06:38 -0600

    renaming evaluators.

--- core-utilities/src/main/java/edu/illinois/cs/cogcomp/core/experiments/evaluators/ConstituentSpanSplittingEvaluator.java
+++ core-utilities/src/main/java/edu/illinois/cs/cogcomp/core/experiments/evaluators/ConstituentSpanSplittingEvaluator.java
@@ -7,7 +7,7 @@ import edu.illinois.cs.cogcomp.core.experiments.ClassificationTester;
 import java.util.HashSet;
 import java.util.Set;
 
-public class SpanSplittingEvaluator extends Evaluator {
+public class ConstituentSpanSplittingEvaluator extends Evaluator {
 
     View gold, prediction;
 
