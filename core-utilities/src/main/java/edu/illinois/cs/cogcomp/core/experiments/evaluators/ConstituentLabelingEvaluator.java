de0cea8805bc3ef75888b0b9c610c361a2a204bd
khashab2 <khashab2@illinois.edu>
2016-02-11 03:06:38 -0600

    renaming evaluators.

--- core-utilities/src/main/java/edu/illinois/cs/cogcomp/core/experiments/evaluators/ConstituentLabelingEvaluator.java
+++ core-utilities/src/main/java/edu/illinois/cs/cogcomp/core/experiments/evaluators/ConstituentLabelingEvaluator.java
@@ -10,7 +10,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-public class SpanLabelingEvaluator extends Evaluator {
+public class ConstituentLabelingEvaluator extends Evaluator {
 
     View gold, prediction;
 
