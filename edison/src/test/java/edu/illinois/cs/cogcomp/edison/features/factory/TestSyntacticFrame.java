bd0881fa598be2a940b2f2b21cbb2c5974824d0e
Christos Christodoulopoulos <christod@illinois.edu>
2016-02-15 17:00:34 -0600

    Minor Edison fixes Updated LBJava dep Removed `CreateTestTAResource`and the dep to `illinois-curator`  - Added a TODO for when we need a new version of `test.ta` (we should use `DummyTextAnnotationGenerator` instead) Few stylistic changes

--- edison/src/test/java/edu/illinois/cs/cogcomp/edison/features/factory/TestSyntacticFrame.java
+++ edison/src/test/java/edu/illinois/cs/cogcomp/edison/features/factory/TestSyntacticFrame.java
@@ -9,16 +9,16 @@ import edu.illinois.cs.cogcomp.core.io.IOUtils;
 import edu.illinois.cs.cogcomp.edison.features.Feature;
 import edu.illinois.cs.cogcomp.edison.features.FeatureExtractor;
 import edu.illinois.cs.cogcomp.edison.utilities.CreateTestFeaturesResource;
-import edu.illinois.cs.cogcomp.edison.utilities.CreateTestTAResource;
 import junit.framework.TestCase;
 
 import java.util.List;
 import java.util.Set;
 
 /**
- * Test class NB: If needed, please re-create the {@code test.ta} and
- * {@code feature.collection.text} files using {@link CreateTestTAResource} and
- * {@link CreateTestFeaturesResource}
+ * Test class
+ * <p/>
+ * <b>NB:</b> If needed, please re-create the {@code feature.collection.text} file using
+ * {@link CreateTestFeaturesResource}.
  *
  * @author Vivek Srikumar
  */
