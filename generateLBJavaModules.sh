d1bcff917581c9fa565c1598e3339885afaead0d
khashab2 <khashab2@illinois.edu>
2016-03-03 16:59:37 -0600

    script for generating and cleaning lbjava files

---
+++ generateLBJavaModules.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+## declare a list of modules which are use lbjava
+declare -a folders=("pos")
+
+## now loop through the above array
+for f in "${folders[@]}"
+do
+    cd "$f"
+    mvn lbjava:generate
+    cd ..
+done
+
+
+
