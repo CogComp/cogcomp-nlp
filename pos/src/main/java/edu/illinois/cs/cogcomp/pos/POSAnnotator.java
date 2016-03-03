320e236d2c17995ef38729982c0727a5e53fbfa8
Christos Christodoulopoulos <christod@illinois.edu>
2016-03-03 12:52:18 -0600

    Added lbjava build instructions in the plugin Applied formatter

--- pos/src/main/java/edu/illinois/cs/cogcomp/pos/POSAnnotator.java
+++ pos/src/main/java/edu/illinois/cs/cogcomp/pos/POSAnnotator.java
@@ -15,12 +15,14 @@ import org.slf4j.LoggerFactory;
 import java.util.List;
 
 /**
- * Wraps the Illinois part-of-speech tagger in an illinois-core-utilites Annotator, to be a pipeline component.
+ * Wraps the Illinois part-of-speech tagger in an illinois-core-utilites Annotator, to be a pipeline
+ * component.
+ * 
  * @author James Clarke, Mark Sammons
  *
  */
 public class POSAnnotator extends Annotator {
-	
+
     private static final String NAME = "illinoispos";
     private final Logger logger = LoggerFactory.getLogger(POSAnnotator.class);
     private final TrainedPOSTagger tagger = new TrainedPOSTagger();
@@ -32,37 +34,38 @@ public class POSAnnotator extends Annotator {
         tokensfield = ViewNames.TOKENS;
         sentencesfield = ViewNames.SENTENCE;
     }
-	
+
 
     /**
-     *  annotates TextAnnotation with POS view and returns the new POS view.
+     * annotates TextAnnotation with POS view and adds it to the TextAnnotation.
      *
-     * @param record    TextAnnotation to annotate
-     * @return  newly created POS view
+     * @param record TextAnnotation to annotate
      */
     @Override
     public void addView(TextAnnotation record) throws AnnotatorException {
-       	if (!record.hasView( tokensfield ) && !record.hasView(sentencesfield)) {
+        if (!record.hasView(tokensfield) && !record.hasView(sentencesfield)) {
             throw new AnnotatorException("Record must be tokenized and sentence split first");
         }
         long startTime = System.currentTimeMillis();
         List<Token> input = LBJavaUtils.recordToLBJTokens(record);
 
 
-        List< Constituent > tokens = record.getView( ViewNames.TOKENS ).getConstituents();
-        TokenLabelView posView = new TokenLabelView( ViewNames.POS, getAnnotatorName(), record, 1.0 );
+        List<Constituent> tokens = record.getView(ViewNames.TOKENS).getConstituents();
+        TokenLabelView posView = new TokenLabelView(ViewNames.POS, getAnnotatorName(), record, 1.0);
         int tcounter = 0;
         for (Token lbjtoken : input) {
             tagger.discreteValue(lbjtoken);
             Constituent token = tokens.get(tcounter);
-            Constituent label = new Constituent(tagger.discreteValue(lbjtoken), ViewNames.POS, record, token.getStartSpan(), token.getEndSpan());
+            Constituent label =
+                    new Constituent(tagger.discreteValue(lbjtoken), ViewNames.POS, record,
+                            token.getStartSpan(), token.getEndSpan());
             posView.addConstituent(label);
             tcounter++;
         }
         long endTime = System.currentTimeMillis();
-        logger.debug("Tagged input in {}ms", endTime-startTime);
+        logger.debug("Tagged input in {}ms", endTime - startTime);
 
-        record.addView( ViewNames.POS, posView );
+        record.addView(ViewNames.POS, posView);
 
     }
 
@@ -75,10 +78,12 @@ public class POSAnnotator extends Annotator {
 
 
     /**
-     * Can be used internally by {@link edu.illinois.cs.cogcomp.annotation.AnnotatorService} to check for pre-requisites before calling
-     * any single (external) {@link edu.illinois.cs.cogcomp.core.datastructures.textannotation.Annotator}.
+     * Can be used internally by {@link edu.illinois.cs.cogcomp.annotation.AnnotatorService} to
+     * check for pre-requisites before calling any single (external)
+     * {@link Annotator}.
      *
-     * @return The list of {@link edu.illinois.cs.cogcomp.core.datastructures.ViewNames} required by this ViewGenerator
+     * @return The list of {@link edu.illinois.cs.cogcomp.core.datastructures.ViewNames} required by
+     *         this ViewGenerator
      */
     @Override
     public String[] getRequiredViews() {
--- pos/src/main/java/edu/illinois/cs/cogcomp/pos/POSTagPlain.java
+++ pos/src/main/java/edu/illinois/cs/cogcomp/pos/POSTagPlain.java
@@ -7,27 +7,21 @@ import edu.illinois.cs.cogcomp.pos.lbjava.*;
 
 
 /**
- * This program uses {@link POSTagger} to tag plain text.  All output is sent
- * to <code>STDOUT</code>.
+ * This program uses {@link POSTagger} to tag plain text. All output is sent to <code>STDOUT</code>.
  *
- * <h4>Usage</h4>
- * <blockquote><code>
+ * <h4>Usage</h4> <blockquote><code>
  *   java edu.illinois.cs.cogcomp.lbj.pos.POSTagPlain &lt;text file&gt;
  * </code></blockquote>
  *
- * <h4>Input</h4>
- * The lone command line parameter is simply the name of a plain text file,
- * written in natural language (i.e., no annotations, and no preprocessing
- * has been performed).
+ * <h4>Input</h4> The lone command line parameter is simply the name of a plain text file, written
+ * in natural language (i.e., no annotations, and no preprocessing has been performed).
  *
- * <h4>Output</h4>
- * The output will contain exactly one sentence per line, and each word will
- * be surrounded by parentheses, accompanied by a POS tag.
+ * <h4>Output</h4> The output will contain exactly one sentence per line, and each word will be
+ * surrounded by parentheses, accompanied by a POS tag.
  *
  * @author Nick Rizzolo
  **/
-public class POSTagPlain
-{
+public class POSTagPlain {
     /**
      * Implements the program described above.
      *
@@ -36,7 +30,8 @@ public class POSTagPlain
     public static void main(String[] args) {
         // Parse the command line
         if (args.length != 1) {
-            System.err.println("usage: java edu.illinois.cs.cogcomp.lbj.pos.POSTagPlain <text file>");
+            System.err
+                    .println("usage: java edu.illinois.cs.cogcomp.lbj.pos.POSTagPlain <text file>");
             System.exit(1);
         }
 
@@ -57,4 +52,3 @@ public class POSTagPlain
         }
     }
 }
-
