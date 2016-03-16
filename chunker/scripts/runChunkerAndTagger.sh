ee50d3480d751f2ef381ddda9252b5531925340c
Nitish Gupta <gnnitish@gmail.com>
2016-03-16 12:36:28 -0500

    Deleted old chunker from December. Will port the lastest chunker

--- chunker/scripts/runChunkerAndTagger.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -Xmx512m -cp dist/*:lib/*:class/* edu.illinois.cs.cogcomp.lbj.chunk.ChunksAndPOSTags $1
--- chunker/scripts/runSegmenter.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-java -cp class/*:dist/*:lib/* edu.illinois.cs.cogcomp.lbjava.nlp.seg.SegmentTagPlain edu.illinois.cs.cogcomp.lbj.chunk.Chunker $1
