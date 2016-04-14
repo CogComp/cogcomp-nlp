6bff217f87aefe9602147bc440360bc6de8b9887
Narender Gupta <ngupta18@illinois.edu>
2016-04-14 13:51:54 -0500

    Added Borda Count method for producing a single ranking from various rankings.

---
+++ core-utilities/src/main/java/edu/illinois/cs/cogcomp/core/algorithms/BordaCount.java
@@ -0,0 +1,44 @@
+package edu.illinois.cs.cogcomp.core.algorithms;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * This is an implementation of Borda Count algorithm for aggregating multiple rankings into a single ranking.
+ *
+ * @author Narender Gupta
+ */
+public class BordaCount {
+
+    /**
+     *
+     * @param rankings A list of rankings of object T.
+     *                 It is assumed that the type <code>T</code> implements the <code>hashCode()</code> and
+     *                 <code>equals()</code> functions.
+     * @return A ranking of objects T, aggregated by simple Borda count algorithm.
+     */
+    public static <T> List<T> getAggregatedRanking(List<List<T>> rankings) {
+        List<T> result = new ArrayList<>();
+        if (rankings != null && rankings.size()>0) {
+            int size = rankings.get(0).size();
+            for (List<T> ranking: rankings) {
+                if (ranking.size() != size)
+                    throw new IllegalArgumentException("Number of element in each ranking should be same.");
+            }
+            Map<T,Integer> scores = new HashMap<>();
+            for (List<T> ranking: rankings) {
+                for (int i=0; i<size; i++) {
+                    int index = size - 1 - i;
+                    T item = ranking.get(index);
+                    if (!scores.containsKey(item))
+                        scores.put(item,0);
+                    scores.put(item, scores.get(item) + i);
+                }
+            }
+            result = Sorters.reverseSortMapByValue(scores);
+        }
+        return result;
+    }
+}
---
+++ core-utilities/src/test/java/edu/illinois/cs/cogcomp/core/algorithms/BordaCountTest.java
@@ -0,0 +1,56 @@
+package edu.illinois.cs.cogcomp.core.algorithms;
+
+import junit.framework.TestCase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Narender Gupta
+ */
+public class BordaCountTest extends TestCase {
+    private List<List<String>> rankings;
+    private List<String> aggregate;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        this.rankings = new ArrayList<>();
+        List<String> ranking1 = new ArrayList<>();
+        ranking1.add("A");
+        ranking1.add("B");
+        ranking1.add("C");
+        ranking1.add("D");
+        this.rankings.add(ranking1);
+        List<String> ranking2 = new ArrayList<>();
+        ranking2.add("D");
+        ranking2.add("B");
+        ranking2.add("C");
+        ranking2.add("A");
+        this.rankings.add(ranking2);
+        List<String> ranking3 = new ArrayList<>();
+        ranking3.add("D");
+        ranking3.add("B");
+        ranking3.add("A");
+        ranking3.add("C");
+        this.rankings.add(ranking3);
+        List<String> ranking4 = new ArrayList<>();
+        ranking4.add("C");
+        ranking4.add("B");
+        ranking4.add("D");
+        ranking4.add("A");
+        this.rankings.add(ranking4);
+
+        this.aggregate = new ArrayList<>();
+        this.aggregate.add("B");
+        this.aggregate.add("D");
+        this.aggregate.add("C");
+        this.aggregate.add("A");
+    }
+
+    public void testSimpleAggregateRanking() {
+        List<String> aggregate = BordaCount.getAggregatedRanking(this.rankings);
+        assert(aggregate != null);
+        assert(aggregate.equals(this.aggregate));
+    }
+}
