4f9bc678fadfe2cc16416345e22d564db0162869
Mazin Bokhari <mbokhar2@illinois.edu>
2016-04-17 20:11:02 -0500

    added expected outputs for TestPOSandPositionWindowThree.java

--- edison/src/test/java/edu/illinois/cs/cogcomp/edison/features/factory/TestPOSandPositionWindowThree.java
+++ edison/src/test/java/edu/illinois/cs/cogcomp/edison/features/factory/TestPOSandPositionWindowThree.java
@@ -1,5 +1,6 @@
 package edu.illinois.cs.cogcomp.edison.features.factory.newfexes;
 
+import org.apache.commons.lang.ArrayUtils;
 import edu.illinois.cs.cogcomp.core.datastructures.ViewNames;
 import edu.illinois.cs.cogcomp.core.datastructures.textannotation.Constituent;
 import edu.illinois.cs.cogcomp.core.datastructures.textannotation.PredicateArgumentView;
@@ -71,14 +72,39 @@ public class TestPOSandPositionWindowThree extends TestCase {
 	//System.out.println("Startspan is "+test.getStartSpan()+" and Endspan is "+test.getEndSpan());
 	
 	Set<Feature> feats = POSWpp.getFeatures(test);
-	
+	String[] expected_outputs = [
+		"POSandPositionWindowThree:0_0(DT)",
+		"POSandPositionWindowThree:1_0(VBZ)",
+		"POSandPositionWindowThree:2_0(DT)",
+		"POSandPositionWindowThree:3_0(NN)",
+		"POSandPositionWindowThree:4_0(.)",
+		"POSandPositionWindowThree:5_0(null)",
+		"POSandPositionWindowThree:6_0(null)",
+		"POSandPositionWindowThree:0_1(DT_VBZ)",
+		"POSandPositionWindowThree:1_1(VBZ_DT)",
+		"POSandPositionWindowThree:2_1(DT_NN)",
+		"POSandPositionWindowThree:3_1(NN_.)",
+		"POSandPositionWindowThree:4_1(._null)",
+		"POSandPositionWindowThree:5_1(null_null)",
+		"POSandPositionWindowThree:6_1(null)",
+		"POSandPositionWindowThree:0_2(DT_VBZ_DT)",
+		"POSandPositionWindowThree:1_2(VBZ_DT_NN)",
+		"POSandPositionWindowThree:2_2(DT_NN_.)",
+		"POSandPositionWindowThree:3_2(NN_._null)",
+		"POSandPositionWindowThree:4_2(._null_null)",
+		"POSandPositionWindowThree:5_2(null_null)",
+		"POSandPositionWindowThree:6_2(null)"
+	];
+
+
 	if(feats == null){
 	    System.out.println("Feats are returning NULL.");
 	}
 	
 	System.out.println("Printing Set of Features");
 	for(Feature f: feats){
-	    System.out.println(f.getName());
+	    // System.out.println(f.getName());
+		assert(ArrayUtils.contains( expected_outputs, f.getName()))
 	}
 	
 	System.out.println("GOT FEATURES YES!");
