ab026c37db9a49a91374e655347ddbd565d13eca
khashab2 <khashab2@illinois.edu>
2016-04-22 14:59:55 -0500

    added license to ner.

--- ner/src/main/java/edu/illinois/cs/cogcomp/ner/LbjFeatures/NETaggerLevel1$$1.java
+++ ner/src/main/java/edu/illinois/cs/cogcomp/ner/LbjFeatures/NETaggerLevel1$$1.java
@@ -1,3 +1,13 @@
+/**
+ * This software is released under the University of Illinois/Research and
+ *  Academic Use License. See the LICENSE file in the root folder for details.
+ * Copyright (c) 2016
+ *
+ * Developed by:
+ * The Cognitive Computation Group
+ * University of Illinois at Urbana-Champaign
+ * http://cogcomp.cs.illinois.edu/
+ */
 // Modifying this comment will cause the next execution of LBJava to overwrite this file.
 // F1B88000000000000000BCDCCA84D415550F37D094C4F4F4D22F94D2B4DC134515134D0F37D0FCF2A415827021A9A063ABA0E69A98525A549A5C01511C9198549A921E99529106EB19E0A9CBF7E5E45AE82000EE857C97D5000000
 
--- ner/src/main/java/edu/illinois/cs/cogcomp/ner/tagger/package-info.java
+++ ner/src/main/java/edu/illinois/cs/cogcomp/ner/tagger/package-info.java
@@ -1,4 +1,14 @@
 /**
+ * This software is released under the University of Illinois/Research and
+ *  Academic Use License. See the LICENSE file in the root folder for details.
+ * Copyright (c) 2016
+ *
+ * Developed by:
+ * The Cognitive Computation Group
+ * University of Illinois at Urbana-Champaign
+ * http://cogcomp.cs.illinois.edu/
+ */
+/**
  * This package contains the command line tagger and associated required files and interfaces. The
  * {@link edu.illinois.cs.cogcomp.ner.tagger.NamedEntityTagger} class is a command line tool
  * allowing it's user to read data form a file or directory and to produce the output(s) likewise to
