e971c466600b92d1c0d9783d7df8c6d7f6d8c723
xinbowu2 <17jasonwu@gmail.com>
2016-05-21 08:09:15 +0800

    New SRL in Edison

---
+++ edison/src/main/java/edu/illinois/cs/cogcomp/edison/features/lrec/srl/Verb/Classifier/ParseSibling.java
@@ -0,0 +1,55 @@
+package edu.illinois.cs.cogcomp.edison.features.lrec.srl.Verb.Classifier;
+
+import edu.illinois.cs.cogcomp.core.datastructures.ViewNames;
+import edu.illinois.cs.cogcomp.core.datastructures.textannotation.Constituent;
+import edu.illinois.cs.cogcomp.edison.features.*;
+import edu.illinois.cs.cogcomp.edison.utilities.EdisonException;
+
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+
+/**
+ * 
+ * @author Xinbo Wu
+ */
+public class ParseSibling implements FeatureExtractor {
+	private final String name;
+	private final FeatureCollection base;
+
+	public ParseSibling(){
+		this("#parseSibling#");
+	}
+
+	public ParseSibling(String name){
+		this.name = name;
+		this.base = new FeatureCollection(this.getName());
+
+		this.base.addFeatureExtractor(new FeatureExtractor() {
+
+			@Override
+			public String getName() {
+				return "";
+			}
+
+			@Override
+			public Set<Feature> getFeatures(Constituent c) throws EdisonException {
+				return new LinkedHashSet<Feature>(Collections.singletonList(DiscreteFeature.create(c.getLabel())));
+			}
+		});
+		
+		this.base.addFeatureExtractor(new ParseHeadWordFeatureExtractor(ViewNames.PARSE_STANFORD, new WordPos("")));
+	}
+	
+	
+	@Override
+	public Set<Feature> getFeatures(Constituent c) throws EdisonException {
+		return base.getFeatures(c);
+	}
+	
+	@Override
+	public String getName() {
+		return this.name;
+	}
+}
---
+++ edison/src/main/java/edu/illinois/cs/cogcomp/edison/features/lrec/srl/Verb/Classifier/WordPos.java
@@ -0,0 +1,43 @@
+package edu.illinois.cs.cogcomp.edison.features.lrec.srl.Verb.Classifier;
+
+import edu.illinois.cs.cogcomp.core.datastructures.textannotation.Constituent;
+import edu.illinois.cs.cogcomp.edison.features.Feature;
+import edu.illinois.cs.cogcomp.edison.features.FeatureCollection;
+import edu.illinois.cs.cogcomp.edison.features.FeatureExtractor;
+import edu.illinois.cs.cogcomp.edison.features.factory.WordFeatureExtractorFactory;
+import edu.illinois.cs.cogcomp.edison.utilities.EdisonException;
+
+import java.util.Set;
+
+
+/**
+ * 
+ * @author Xinbo Wu
+ */
+public class WordPos implements FeatureExtractor {
+	private final String name;
+	private final FeatureCollection base;
+
+	public WordPos(){
+		this("#wordPos#");
+	}
+
+	public WordPos(String name){
+		this.name = name;
+		this.base = new FeatureCollection(this.getName());
+
+		this.base.addFeatureExtractor(WordFeatureExtractorFactory.word);
+		this.base.addFeatureExtractor(WordFeatureExtractorFactory.pos);
+	}
+	
+	
+	@Override
+	public Set<Feature> getFeatures(Constituent c) throws EdisonException {
+		return base.getFeatures(c);
+	}
+	
+	@Override
+	public String getName() {
+		return this.name;
+	}
+}
