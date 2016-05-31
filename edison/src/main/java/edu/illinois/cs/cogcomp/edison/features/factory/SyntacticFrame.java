5ba7963b2d7e16ff7e07d1af4b405b3726adf0da
khashab2 <khashab2@illinois.edu>
2016-05-31 10:32:48 -0700

    extract features only when incoming relations are not empty.

--- edison/src/main/java/edu/illinois/cs/cogcomp/edison/features/factory/SyntacticFrame.java
+++ edison/src/main/java/edu/illinois/cs/cogcomp/edison/features/factory/SyntacticFrame.java
@@ -13,6 +13,7 @@ package edu.illinois.cs.cogcomp.edison.features.factory;
 import edu.illinois.cs.cogcomp.core.datastructures.IntPair;
 import edu.illinois.cs.cogcomp.core.datastructures.ViewNames;
 import edu.illinois.cs.cogcomp.core.datastructures.textannotation.Constituent;
+import edu.illinois.cs.cogcomp.core.datastructures.textannotation.Relation;
 import edu.illinois.cs.cogcomp.core.datastructures.textannotation.TextAnnotation;
 import edu.illinois.cs.cogcomp.core.datastructures.textannotation.TreeView;
 import edu.illinois.cs.cogcomp.edison.features.DiscreteFeature;
@@ -23,6 +24,7 @@ import edu.illinois.cs.cogcomp.edison.utilities.EdisonException;
 import edu.illinois.cs.cogcomp.nlp.utilities.ParseTreeProperties;
 
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -45,59 +47,59 @@ public class SyntacticFrame implements FeatureExtractor {
 
     @Override
     public Set<Feature> getFeatures(Constituent c) throws EdisonException {
-
-        Constituent pred = c.getIncomingRelations().get(0).getSource();
-
-        TextAnnotation ta = c.getTextAnnotation();
-
-        TreeView parse = (TreeView) ta.getView(parseViewName);
-
-        Constituent predicate, arg;
-        try {
-            predicate = parse.getParsePhrase(pred);
-            arg = parse.getParsePhrase(c);
-        } catch (Exception e) {
-            throw new EdisonException(e);
-        }
-
-        Constituent vp = TreeView.getParent(predicate);
-
-        // go over VP's siblings before it
-
-        StringBuffer sb1 = new StringBuffer();
-
-        StringBuffer sb2 = new StringBuffer();
-
-        StringBuffer sb3 = new StringBuffer();
-
-        if (!TreeView.isRoot(vp)) {
-            Constituent vpParent = TreeView.getParent(vp);
-
-            for (int i = 0; i < vpParent.getOutgoingRelations().size(); i++) {
-                Constituent target = vpParent.getOutgoingRelations().get(i).getTarget();
-                if (target == vp)
-                    break;
-                addToFeature(target, arg, sb1, sb2, sb3);
-            }
-        }
-
-        for (int i = 0; i < vp.getOutgoingRelations().size(); i++) {
-            Constituent target = vp.getOutgoingRelations().get(i).getTarget();
-
-            if (target.getSpan().equals(predicate.getSpan())) {
-                sb1.append("v-");
-                sb2.append("v-");
-                sb3.append(WordHelpers.getLemma(ta, target.getStartSpan())).append("-");
-            } else {
-                addToFeature(target, arg, sb1, sb2, sb3);
-            }
-        }
-
         Set<Feature> features = new LinkedHashSet<>();
-        features.add(DiscreteFeature.create(sb1.toString()));
-        features.add(DiscreteFeature.create("general:" + sb2.toString()));
-        features.add(DiscreteFeature.create("lemma:" + sb3.toString()));
+        List<Relation> incomingRelations = c.getIncomingRelations();
 
+        if(incomingRelations.size() > 0) {
+            Constituent pred = incomingRelations.get(0).getSource();
+            TextAnnotation ta = c.getTextAnnotation();
+            TreeView parse = (TreeView) ta.getView(parseViewName);
+
+            Constituent predicate, arg;
+            try {
+                predicate = parse.getParsePhrase(pred);
+                arg = parse.getParsePhrase(c);
+            } catch (Exception e) {
+                throw new EdisonException(e);
+            }
+
+            Constituent vp = TreeView.getParent(predicate);
+
+            // go over VP's siblings before it
+
+            StringBuffer sb1 = new StringBuffer();
+
+            StringBuffer sb2 = new StringBuffer();
+
+            StringBuffer sb3 = new StringBuffer();
+
+            if (!TreeView.isRoot(vp)) {
+                Constituent vpParent = TreeView.getParent(vp);
+
+                for (int i = 0; i < vpParent.getOutgoingRelations().size(); i++) {
+                    Constituent target = vpParent.getOutgoingRelations().get(i).getTarget();
+                    if (target == vp)
+                        break;
+                    addToFeature(target, arg, sb1, sb2, sb3);
+                }
+            }
+
+            for (int i = 0; i < vp.getOutgoingRelations().size(); i++) {
+                Constituent target = vp.getOutgoingRelations().get(i).getTarget();
+
+                if (target.getSpan().equals(predicate.getSpan())) {
+                    sb1.append("v-");
+                    sb2.append("v-");
+                    sb3.append(WordHelpers.getLemma(ta, target.getStartSpan())).append("-");
+                } else {
+                    addToFeature(target, arg, sb1, sb2, sb3);
+                }
+            }
+
+            features.add(DiscreteFeature.create(sb1.toString()));
+            features.add(DiscreteFeature.create("general:" + sb2.toString()));
+            features.add(DiscreteFeature.create("lemma:" + sb3.toString()));
+        }
         return features;
     }
 
