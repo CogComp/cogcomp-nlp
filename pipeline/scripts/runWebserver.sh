dadb52776bbaa504ee74d5a93cb5346acb0e7785
khashab2 <khashab2@illinois.edu>
2016-12-14 14:29:00 -0600

    adding the memory settings.

--- pipeline/scripts/runWebserver.sh
+++ pipeline/scripts/runWebserver.sh
@@ -1,3 +1,5 @@
 #!/bin/bash -e
 
- mvn -pl pipeline compile  exec:java -Dexec.mainClass=edu.illinois.cs.cogcomp.pipeline.server.MainServer -DargLine="-Xmx10g"
+export MAVEN_OPTS="-Xmx35g"
+
+mvn -pl pipeline compile  exec:java -Dexec.mainClass=edu.illinois.cs.cogcomp.pipeline.server.MainServer # -DargLine="-Xmx10g"
--- pipeline/src/main/java/edu/illinois/cs/cogcomp/pipeline/server/MainServer.java
+++ pipeline/src/main/java/edu/illinois/cs/cogcomp/pipeline/server/MainServer.java
@@ -2,6 +2,7 @@ package edu.illinois.cs.cogcomp.pipeline.server;
 
 import edu.illinois.cs.cogcomp.annotation.AnnotatorException;
 import edu.illinois.cs.cogcomp.annotation.AnnotatorService;
+import edu.illinois.cs.cogcomp.core.datastructures.ViewNames;
 import edu.illinois.cs.cogcomp.core.datastructures.textannotation.TextAnnotation;
 import edu.illinois.cs.cogcomp.core.utilities.SerializationHelper;
 import edu.illinois.cs.cogcomp.core.utilities.configuration.ResourceManager;
@@ -17,8 +18,32 @@ public class MainServer {
     public static void main(String[] args) {
         AnnotatorService pipeline = null;
         try {
+            System.out.println("Starting to load the pipeline . . . ");
+
+            int mb = 1024*1024;
+
+            //Getting the runtime reference from system
+            Runtime runtime = Runtime.getRuntime();
+
+            System.out.println("##### Heap utilization statistics [MB] #####");
+
+            //Print used memory
+            System.out.println("Used Memory:"
+                    + (runtime.totalMemory() - runtime.freeMemory()) / mb);
+
+            //Print free memory
+            System.out.println("Free Memory:"
+                    + runtime.freeMemory() / mb);
+
+            //Print total available memory
+            System.out.println("Total Memory:" + runtime.totalMemory() / mb);
+
+            //Print Maximum available memory
+            System.out.println("Max Memory:" + runtime.maxMemory() / mb);
+
             ResourceManager rm = new PipelineConfigurator().getDefaultConfig();
             pipeline = PipelineFactory.buildPipeline(rm);
+            System.out.println("Done with loadin gthe pipeline  . . .");
         } catch (IOException | AnnotatorException e) {
             e.printStackTrace();
         }
@@ -33,8 +58,13 @@ public class MainServer {
             }
             else {
                 String[] viewsInArray = views.split(",");
+                System.out.println("Doing the basic ");
                 TextAnnotation ta = finalPipeline.createBasicTextAnnotation( "", "", text);
+                System.out.println("done with the basic");
+                finalPipeline.addView(ta, ViewNames.NER_CONLL);
+                System.out.println("serializing");
                 output = SerializationHelper.serializeToJson(ta);
+                System.out.println("done with all");
             }
             return output;
         });
