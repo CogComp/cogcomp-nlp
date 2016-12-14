72836d549ee80f1e45b0c648e89d22bb6798f616
khashab2 <khashab2@illinois.edu>
2016-12-14 14:54:50 -0600

    minor formatting.

--- pipeline/src/main/java/edu/illinois/cs/cogcomp/pipeline/server/MainServer.java
+++ pipeline/src/main/java/edu/illinois/cs/cogcomp/pipeline/server/MainServer.java
@@ -36,17 +36,18 @@ public class MainServer {
             String views = request.queryParams("views");
             String output = "";
             if(views == null || text == null) {
-                output = "The parameters 'text' and/or 'views' are not specified. Here is a sample input:  \n ?text=\"This is a sample sentence. I'm happy.\"&views=\"pos,ner\"";
+                output = "The parameters 'text' and/or 'views' are not specified. Here is a sample input:  \n ?text=\"This is a sample sentence. I'm happy.\"&views=POS,NER";
             }
             else {
+                System.out.println("------------------------------");
                 System.out.println("Text: " + text);
                 System.out.println("Views to add: " + views);
                 String[] viewsInArray = views.split(",");
                 System.out.println("Adding the basic annotations . . . ");
                 TextAnnotation ta = finalPipeline.createBasicTextAnnotation("", "", text);
                 for(String vuName : viewsInArray) {
-                    System.out.println("Adding the view" + vuName);
-                    finalPipeline.addView(ta, vuName);
+                    System.out.println("Adding the view: ->" + vuName.trim()+ "<-");
+                    finalPipeline.addView(ta, vuName.trim());
                     printMemoryDetails();
                 }
                 System.out.println("Done adding the views. Deserializing the view now.");
@@ -71,10 +72,10 @@ public class MainServer {
         System.out.print("##### Used Memory[MB]:" + (runtime.totalMemory() - runtime.freeMemory()) / mb);
 
         //Print free memory
-        System.out.println(" / Free Memory[MB]:" + runtime.freeMemory() / mb);
+        System.out.print(" / Free Memory[MB]:" + runtime.freeMemory() / mb);
 
         //Print total available memory
-        System.out.print("##### Total Memory[MB]:" + runtime.totalMemory() / mb);
+        System.out.print(" / Total Memory[MB]:" + runtime.totalMemory() / mb);
 
         //Print Maximum available memory
         System.out.println(" / Max Memory[MB]:" + runtime.maxMemory() / mb);
