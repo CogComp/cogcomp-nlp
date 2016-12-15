962f650a842169f675092cd7e19fc13af6fe77f3
khashab2 <khashab2@illinois.edu>
2016-12-14 21:28:33 -0600

    adding license header.

--- pipeline/src/main/java/edu/illinois/cs/cogcomp/pipeline/server/MainServer.java
+++ pipeline/src/main/java/edu/illinois/cs/cogcomp/pipeline/server/MainServer.java
@@ -1,8 +1,14 @@
+/**
+ * This software is released under the University of Illinois/Research and Academic Use License. See
+ * the LICENSE file in the root folder for details. Copyright (c) 2016
+ *
+ * Developed by: The Cognitive Computation Group University of Illinois at Urbana-Champaign
+ * http://cogcomp.cs.illinois.edu/
+ */
 package edu.illinois.cs.cogcomp.pipeline.server;
 
 import edu.illinois.cs.cogcomp.annotation.AnnotatorException;
 import edu.illinois.cs.cogcomp.annotation.AnnotatorService;
-import edu.illinois.cs.cogcomp.core.datastructures.ViewNames;
 import edu.illinois.cs.cogcomp.core.datastructures.textannotation.TextAnnotation;
 import edu.illinois.cs.cogcomp.core.utilities.SerializationHelper;
 import edu.illinois.cs.cogcomp.core.utilities.configuration.ResourceManager;
