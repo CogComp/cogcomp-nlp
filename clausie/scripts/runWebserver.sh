9d7062c3b6c022705cdf0efdd89bdf434f273511
khashab2 <khashab2@illinois.edu>
2017-06-07 19:23:42 -0400

    exclude stanford from clause.

--- clausie/scripts/runWebserver.sh
+++ clausie/scripts/runWebserver.sh
@@ -2,4 +2,4 @@
 
 export MAVEN_OPTS="-Xmx10g"
 
-mvn -pl external compile  exec:java -Dexec.mainClass=edu.illinois.cs.cogcomp.pipeline.server.ClausIEServer -Dexec.args="$*" # -DargLine="-Xmx10g"
+mvn -pl clausie compile  exec:java -Dexec.mainClass=edu.illinois.cs.cogcomp.pipeline.handlers.ClausIEServer -Dexec.args="$*" # -DargLine="-Xmx10g"
