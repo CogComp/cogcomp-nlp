6788e85155447a6cd3e1adcb62e950623b6c8da6
Mark Sammons <mssammon@illinois.edu>
2017-07-02 16:23:17 -0500

    improve flexibility of eredocumentreader (allow for kbp corpus directory structure) tried to make CuratorClient more robust

--- core-utilities/src/test/java/edu/illinois/cs/cogcomp/core/datastructures/textannotation/TextAnnotationUtilitiesTest.java
+++ core-utilities/src/test/java/edu/illinois/cs/cogcomp/core/datastructures/textannotation/TextAnnotationUtilitiesTest.java
@@ -20,12 +20,17 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 public class TextAnnotationUtilitiesTest {
+
+    /**
+     * note that this test checks getSubTextAnnotation()'s behavior when a sentence has no constituents in
+     *   a particular view for a particular sentence (view is not generated in sentence level version)
+     */
     @Test
     public void test() {
         TextAnnotation ta = DummyTextAnnotationGenerator.generateAnnotatedTextAnnotation(false, 3);
         TextAnnotation subTA = TextAnnotationUtilities.getSubTextAnnotation(ta, 2);
         assertTrue(subTA.getText().equals("The paving commenced Monday and will finish in June ."));
-        assertTrue(Objects.equals(subTA.getAvailableViews().toString(), "[SRL_VERB, PSEUDO_PARSE_STANFORD, POS, NER_CONLL, LEMMA, SHALLOW_PARSE, TOKENS, SENTENCE, PARSE_GOLD]"));
+        assertTrue(Objects.equals(subTA.getAvailableViews().toString(), "[SRL_VERB, PSEUDO_PARSE_STANFORD, POS, LEMMA, SHALLOW_PARSE, TOKENS, SENTENCE, PARSE_GOLD]"));
         assertTrue(Objects.equals(subTA.getView(ViewNames.SHALLOW_PARSE).toString(), "[NP The paving ] [VP commenced ] [NP Monday ] [VP will finish ] [PP in ] [NP June ] "));
         String parse = "(S1 (S (NP (DT The)\n" +
                 "    (NN paving))\n" +
