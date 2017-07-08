711ebaff5f5af5960cecccfd4b74a954fb44c451
Slash0BZ <zhouxuanyu.personal@gmail.com>
2017-07-08 09:29:05 -0700

    add BIOReader

---
+++ src/main/java/org/cogcomp/md/BIOReader.java
@@ -0,0 +1,86 @@
+package org.cogcomp.md;
+import java.util.*;
+import edu.illinois.cs.cogcomp.lbjava.parse.Parser;
+import edu.illinois.cs.cogcomp.nlp.corpusreaders.ACEReader;
+import edu.illinois.cs.cogcomp.core.datastructures.textannotation.*;
+import edu.illinois.cs.cogcomp.core.datastructures.ViewNames;
+
+public class BIOReader implements Parser
+{
+    private List<Constituent> tokenList;
+    private int tokenIndex;
+    private List<TextAnnotation> taList;
+    private String _path;
+    private String _mode;
+
+    public BIOReader(String path, String mode){
+        _path = path;
+        _mode = mode;
+        taList = getTextAnnotations();
+        tokenList = getTokensFromTAs();
+    }
+
+    private List<TextAnnotation> getTextAnnotations(){
+        List<TextAnnotation> ret = new ArrayList<>();
+        if (_mode.equals("ACE05")){
+            ACEReader aceReader = null;
+            try{
+                aceReader = new ACEReader(_path, false);
+            }
+            catch (Exception e){
+                e.printStackTrace();
+            }
+            for (TextAnnotation ta : aceReader) {
+                ret.add(ta);
+            }
+        }
+        else{
+            System.out.println("No defult actions for unknown mode");
+        }
+        return ret;
+    }
+
+    private List<Constituent> getTokensFromTAs(){
+        List<Constituent> ret = new ArrayList<>();
+        if (_mode.equals("ACE05")){
+            for (TextAnnotation ta : taList){
+                View tokenView = ta.getView(ViewNames.TOKENS);
+                View mentionView = ta.getView(ViewNames.MENTION_ACE);
+                String[] token2tags = new String[tokenView.getConstituents().size()];
+                for (int i = 0; i < token2tags.length; i++){
+                    token2tags[i] = "O";
+                }
+                for (Constituent c : mentionView.getConstituents()){
+                    token2tags[c.getStartSpan()] = "B";
+                    for (int i = c.getStartSpan() + 1; i < c.getEndSpan(); i++){
+                        token2tags[i] = "I";
+                    }
+                }
+                for (int i = 0; i < token2tags.length; i++){
+                    Constituent curToken = tokenView.getConstituentsCoveringToken(i).get(0);
+                    Constituent newToken = curToken.cloneForNewView("MD");
+                    newToken.addAttribute("BIO", token2tags[i]);
+                    ret.add(newToken);
+                }
+            }
+        }
+        else{
+            System.out.println("No defult actions for unknown mode");
+        }
+        return ret;
+    }
+
+    public void close(){}
+    public Object next(){
+        if (tokenIndex == tokenList.size()) {
+            return null;
+        } else {
+            tokenIndex++;
+            return tokenList.get(tokenIndex - 1);
+        }
+    }
+
+    public void reset(){
+        tokenIndex = 0;
+    }
+}
