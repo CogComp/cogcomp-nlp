b682c126509c24c0dc8029b918f4c6c7534fd127
zfeng6 <zfeng6@illinois.edu>
2017-07-10 21:38:59 -0700

    delete ner lbjfeatures

--- chunker/scripts/mvn_train.sh
+++ chunker/scripts/mvn_train.sh
@@ -2,14 +2,14 @@
 #----Please make sure "illinois-chunker-model" is removed from the dependency list in pom.xml----#
 
 #   Specify the training set
-TRAINFILE=/Users/zhilifeng/Desktop/DanRothResearch/illinois-cogcomp-nlp/chunker/TBAQ_full_1label_corr.txt
+TRAINFILE=/shared/corpora/corporaWeb/written/eng/chunking/conll2000distributions/train.txt
 
 #   Specify the dir and name to save the resulting model
 MODELDIR=model
 MODELNAME=mychunker
 
 #   Specify the training round
-ROUND=50
+ROUND=5
 #ROUND=50
 
 mkdir -p $MODELDIR
