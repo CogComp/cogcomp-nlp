5958df7daa507e42590386ad53e77bf2bb87b2de
Mark Sammons <mssammon@illinois.edu>
2017-08-04 00:39:12 -0500

    fixed merge errror

--- core-utilities/src/main/java/edu/illinois/cs/cogcomp/core/datastructures/textannotation/TextAnnotationUtilities.java
+++ core-utilities/src/main/java/edu/illinois/cs/cogcomp/core/datastructures/textannotation/TextAnnotationUtilities.java
@@ -262,11 +262,8 @@ public class TextAnnotationUtilities {
             if (vu instanceof TreeView) {
                 ((TreeView) newVu).makeTrees();
             }
-<<<<<<< HEAD
             else if (vu instanceof PredicateArgumentView)
                 ((PredicateArgumentView) vu).findPredicates();
-=======
->>>>>>> origin
         }
     }
 
@@ -302,11 +299,7 @@ public class TextAnnotationUtilities {
      * @param offset the offset to shift token indexes of new Constituent. Can be negative.
      * @return the new Constituent
      */
-<<<<<<< HEAD
     public static Constituent copyConstituentWithNewTokenOffsets(TextAnnotation newTA, Constituent c, int offset) {
-=======
-    private static Constituent copyConstituentWithNewTokenOffsets(TextAnnotation newTA, Constituent c, int offset) {
->>>>>>> origin
         int newStart = c.getStartSpan() + offset;
         int newEnd = c.getEndSpan() + offset;
 
@@ -365,12 +358,8 @@ public class TextAnnotationUtilities {
                 updatedTokenCharOffsets, ta.getTokens(), updatedSentenceEndPositions);
 
         for (String vuName : ta.getAvailableViews()) {
-<<<<<<< HEAD
-
             if (ViewNames.TOKENS.equals(vuName) || ViewNames.SENTENCE.equals(vuName))
                 continue;
-=======
->>>>>>> origin
             View vu = ta.getView(vuName);
 
             View newVu = null;
