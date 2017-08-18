f1c535b399d81c10cac89d5c17af39ca698e5c9d
khashab2 <khashab2@illinois.edu>
2017-08-18 11:36:35 -0400

    minor.

--- external/runAllExternalAnnotators.py
+++ external/runAllExternalAnnotators.py
@@ -1,11 +1,11 @@
 ####
-# A simple script to run all the servers of the external annotators on different ports.
-# it first selects a bunch of open ports, and then tries running the modules on the free ports
-# designed for Python 2.7
+# A simple script to list the available ports on the current server
+# designed to work for Python 3.5
 ####
 
 from __future__ import print_function
 import os
+import re
 import sys
 import socket
 from subprocess import check_output
@@ -25,18 +25,14 @@ def checkPortAvailability(port):
 
 # full command in linux: netstat -lnt | awk '$6 == "LISTEN" && $4 ~ ".445"'
 def getUsedPorts():
-    output = check_output(["netstat", "-lnt"])
-    items_split = [ re.sub(' +',' ', i).split(' ') for i in output.split('\n')]
-    return set([x[3].split(":")[-1] for x in items_split if len(x) == 7 and x[5] == 'LISTEN'])
+    output = str(check_output(["netstat", "-lnt"]))
+    items_split = [ re.sub(' +',' ', i).split(' ') for i in output.split('\\n')]
+    return [int(x[3].split(":")[-1]) for x in items_split if len(x) == 7 and x[5] == 'LISTEN']
 
 def main():
     portsUsed = getUsedPorts()
-    filteredPorts = [p for p in ports if p in portsUsed]
-    assert len(modules) <= len(filteredPorts), "there are less number of open ports to be used for modules"
-    for index, elem in enumerate(modules):
-        port = filteredPorts[index]
-        print("running module `" + elem + "` on port `" + port + "`")
-        check_output([elem + "/scripts/runWebserver.sh", "-p", port])
+    filteredPorts = [p for p in ports if p not in portsUsed]
+    print("list of open ports: " + str(filteredPorts))
 
 if __name__ == '__main__':
     sys.exit(main())
