9ff920fac01f68287f4ea0108cc92ce28f5b5666
Mark Sammons <marksdsammons@gmail.com>
2018-02-13 22:55:52 -0600

    Added MASC corpus reader for tokens and sentences. Updated some elements of Reader class hierarchy to address issues that arose during MASC implementation.

---
+++ corpusreaders/src/main/java/edu/illinois/cs/cogcomp/nlp/corpusreaders/mascReader/MascReaderConfigurator.java
@@ -0,0 +1,47 @@
+/**
+ * This software is released under the University of Illinois/Research and Academic Use License. See
+ * the LICENSE file in the root folder for details. Copyright (c) 2016
+ *
+ * Developed by: The Cognitive Computation Group University of Illinois at Urbana-Champaign
+ * http://cogcomp.cs.illinois.edu/
+ */
+package edu.illinois.cs.cogcomp.nlp.corpusreaders.mascReader;
+
+import edu.illinois.cs.cogcomp.core.utilities.configuration.Property;
+import edu.illinois.cs.cogcomp.core.utilities.configuration.ResourceManager;
+import edu.illinois.cs.cogcomp.nlp.corpusreaders.CorpusReaderConfigurator;
+
+/**
+ * Configuration options for MASC readers
+ *
+ * @author mssammon
+ */
+public class MascReaderConfigurator extends CorpusReaderConfigurator{
+
+    public static final Property GENRES = new Property("genres",
+            "blog,email,essays,ficlets,fiction,govt-docs,jokes,journal,letters,movie-script,newspaper,non-fiction," +
+                    "technical,travel-guides,twitter");
+//    public static final Property SOURCES = new Property("sources", "nyt");
+    public static final Property ANNOTATIONS = new Property("annotations", "SENTENCE,TOKENS");
+    public static final Property READ_PENN = new Property("readPenn", FALSE);
+
+
+
+    @Override
+    public ResourceManager getDefaultConfig() {
+        Property[] properties = new Property[]{
+                GENRES,
+//                SOURCES,
+                ANNOTATIONS,
+                READ_PENN,
+                new Property(CorpusReaderConfigurator.CORPUS_NAME.key, "MASC"),
+                new Property(CorpusReaderConfigurator.CORPUS_DIRECTORY.key, "/home/mssammon/work/data/masc-3.0.0/written/"),
+                new Property(CorpusReaderConfigurator.SOURCE_DIRECTORY.key, "/home/mssammon/work/data/masc-3.0.0/written/"),
+                new Property(CorpusReaderConfigurator.SUPPRESS_FILE_ERRORS.key, TRUE),
+        };
+
+        return new ResourceManager(generateProperties(properties));
+    }
+
+
+}
