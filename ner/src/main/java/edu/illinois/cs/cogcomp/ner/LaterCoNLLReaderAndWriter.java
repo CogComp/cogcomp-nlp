fe61282d266a662447bbc0d8be4fc21ca57c82b8
Thomas L. Redman <redman@ncsa.uiuc.edu>
2018-02-15 11:06:18 -0600

    These files do the stanford evaluation given the CoNLL format gold label data.

---
+++ ner/src/main/java/edu/illinois/cs/cogcomp/ner/LaterCoNLLReaderAndWriter.java
@@ -0,0 +1,281 @@
+/**
+ * 
+ */
+package edu.illinois.cs.cogcomp.ner;
+
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.io.IOUtils;
+import edu.stanford.nlp.io.RuntimeIOException;
+import edu.stanford.nlp.objectbank.ObjectBank;
+import edu.stanford.nlp.sequences.DocumentReaderAndWriter;
+import edu.stanford.nlp.sequences.IOBUtils;
+import edu.stanford.nlp.sequences.SeqClassifierFlags;
+import edu.stanford.nlp.stats.ClassicCounter;
+import edu.stanford.nlp.stats.Counter;
+import edu.stanford.nlp.stats.Counters;
+import edu.stanford.nlp.util.AbstractIterator;
+
+import java.util.*;
+import java.util.regex.*;
+import java.io.*;
+
+/**
+ * DocumentReader for the original CoNLL 03 format.  In this format, there is
+ * one word per line, with extra attributes of a word (POS tag, chunk, etc.) in
+ * other space or tab separated columns, where leading and trailing whitespace
+ * on the line are ignored.  Sentences are supposedly
+ * separated by a blank line (one with no non-whitespace characters), but
+ * where blank lines occur is in practice often fairly random. In particular,
+ * sometimes entities span blank lines.  Nevertheless, in this class, like in
+ * our original CoNLL system, these blank lines are preserved as a special
+ * BOUNDARY token and detected and exploited by some features. The text is
+ * divided into documents at each '-DOCSTART-' token, which is seen as a
+ * special token, which is also preserved.  The reader can read data in any
+ * of the IOB/IOE/etc. formats and output tokens in any other, based on the
+ * entitySubclassification flag.
+ * <p>
+ * This reader is specifically for replicating CoNLL systems. For normal use,
+ * you should use the saner ColumnDocumentReaderAndWriter.
+ *
+ * @author Jenny Finkel
+ * @author Huy Nguyen
+ * @author Christopher Manning
+ */
+public class LaterCoNLLReaderAndWriter implements DocumentReaderAndWriter<CoreLabel> {
+
+    private static final long serialVersionUID = 6281374154299530460L;
+
+    public static final String BOUNDARY = "*BOUNDARY*";
+    /** Historically, this reader used to treat the whole input as one document, but now it doesn't */
+    private static final boolean TREAT_FILE_AS_ONE_DOCUMENT = false;
+    private static final Pattern docPattern = Pattern.compile("^\\s*-DOCSTART-\\s");
+    private static final Pattern white = Pattern.compile("^\\s*$");
+
+    private SeqClassifierFlags flags; // = null;
+    private boolean keepgold = true;
+    public LaterCoNLLReaderAndWriter() {
+    }
+    public LaterCoNLLReaderAndWriter(boolean keepgold) {
+        this.keepgold = keepgold;
+    }
+    @Override
+    public void init(SeqClassifierFlags flags) {
+        this.flags = flags;
+    }
+
+    @Override
+    public String toString() {
+        return "LaterCoNLLReaderAndWriter[entitySubclassification: " + flags.entitySubclassification + ", intern: " + flags.intern + ']';
+    }
+
+    @Override
+    public Iterator<List<CoreLabel>> getIterator(Reader r) {
+        return new CoNLLIterator(r);
+    }
+
+    private class CoNLLIterator extends AbstractIterator<List<CoreLabel>> {
+
+        public CoNLLIterator(Reader r) {
+            stringIter = splitIntoDocs(r);
+        }
+
+        @Override
+        public boolean hasNext() {
+            return stringIter.hasNext();
+        }
+
+        @Override
+        public List<CoreLabel> next() {
+            return processDocument(stringIter.next());
+        }
+
+        private Iterator<String> stringIter; // = null;
+
+    } // end class CoNLLIterator
+
+    private static Iterator<String> splitIntoDocs(Reader r) {
+        if (TREAT_FILE_AS_ONE_DOCUMENT) {
+            return Collections.singleton(IOUtils.slurpReader(r)).iterator();
+        } else {
+            Collection<String> docs = new ArrayList<>();
+            ObjectBank<String> ob = ObjectBank.getLineIterator(r);
+            StringBuilder current = new StringBuilder();
+            for (String line : ob) {
+                if (docPattern.matcher(line).lookingAt()) {
+                    // Start new doc, store old one if non-empty
+                    if (current.length() > 0) {
+                        docs.add(current.toString());
+                        current = new StringBuilder();
+                    }
+                }
+                current.append(line);
+                current.append('\n');
+            }
+            if (current.length() > 0) {
+                docs.add(current.toString());
+            }
+            return docs.iterator();
+        }
+    }
+
+    private List<CoreLabel> processDocument(String doc) {
+        List<CoreLabel> list = new ArrayList<>();
+        String[] lines = doc.split("\n");
+        for (String line : lines) {
+            if (line.contains("-DOCSTART-"))
+                continue;
+            if (!flags.deleteBlankLines || !white.matcher(line).matches()) {
+                list.add(makeCoreLabel(line));
+            }
+        }
+        IOBUtils.entitySubclassify(list, CoreAnnotations.AnswerAnnotation.class, flags.backgroundSymbol, flags.entitySubclassification, flags.intern);
+        return list;
+    }
+
+    /** This deals with the CoNLL files for different languages which have
+     *  between 2 and 5 columns on non-blank lines.
+     *
+     *  @param line A line of CoNLL input
+     *  @return The constructed token
+     */
+    private CoreLabel makeCoreLabel(String line) {
+        CoreLabel wi = new CoreLabel();
+        // wi.line = line;
+        String[] bits = line.split("\\s+");
+        switch (bits.length) {
+            case 0:
+            case 1:
+                wi.setWord(BOUNDARY);
+                wi.set(CoreAnnotations.AnswerAnnotation.class, flags.backgroundSymbol);
+                break;
+            default:
+                wi.setWord(bits[5]);
+                wi.setTag(bits[4]);
+                wi.set(CoreAnnotations.AnswerAnnotation.class, bits[0]);
+                break;
+        }
+
+        //Value annotation is used in a lot of place in corenlp so setting here as the word itself
+        wi.set(CoreAnnotations.ValueAnnotation.class, wi.word());
+
+        // The copy to GoldAnswerAnnotation is done before the recoding is done, and so it preserves the original coding.
+        // This is important if the original coding is true, but the recoding is defective (like IOB2 to IO), since
+        // it will allow correct evaluation later.
+        if (keepgold)
+            wi.set(CoreAnnotations.GoldAnswerAnnotation.class, wi.get(CoreAnnotations.AnswerAnnotation.class));
+        return wi;
+    }
+
+    /** Return the coding scheme to IOB1 coding, regardless of what was used
+     *  internally (unless retainEntitySubclassification is set).
+     *  This is useful for scoring against CoNLL test output.
+     *
+     *  @param tokens List of tokens in some NER encoding
+     */
+    private void deEndify(List<CoreLabel> tokens) {
+        if (flags.retainEntitySubclassification) {
+            return;
+        }
+        IOBUtils.entitySubclassify(tokens, CoreAnnotations.AnswerAnnotation.class, flags.backgroundSymbol, "iob1", flags.intern);
+    }
+
+    /** Write a standard CoNLL format output file.
+     *
+     *  @param doc The document: A List of CoreLabel
+     *  @param out Where to send the answers to
+     */
+    @Override
+    @SuppressWarnings({ "StringEquality", "StringContatenationInLoop" })
+    public void printAnswers(List<CoreLabel> doc, PrintWriter out) {
+        // boolean tagsMerged = flags.mergeTags;
+        // boolean useHead = flags.splitOnHead;
+
+        if (!"iob1".equalsIgnoreCase(flags.entitySubclassification)) {
+            deEndify(doc);
+        }
+
+        for (CoreLabel fl : doc) {
+            String word = fl.word();
+            if (word == BOUNDARY) { // Using == is okay, because it is set to constant
+                out.println();
+            } else {
+                String gold = fl.getString(CoreAnnotations.GoldAnswerAnnotation.class);
+                String guess = fl.get(CoreAnnotations.AnswerAnnotation.class);
+                // log.info(word + "\t" + gold + "\t" + guess));
+                String pos = fl.getString(CoreAnnotations.PartOfSpeechAnnotation.class);
+                String chunk = fl.getString(CoreAnnotations.ChunkAnnotation.class);
+                out.println(fl.word() + '\t' + pos + '\t' + chunk + '\t' + gold + '\t' + guess);
+            }
+        }
+    }
+
+    private static StringBuilder maybeIncrementCounter(StringBuilder inProgressMisc, Counter<String> miscCounter) {
+        if (inProgressMisc.length() > 0) {
+            miscCounter.incrementCount(inProgressMisc.toString());
+            inProgressMisc = new StringBuilder();
+        }
+        return inProgressMisc;
+    }
+
+    /** Count some stats on what occurs in a file.
+     */
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        LaterCoNLLReaderAndWriter rw = new LaterCoNLLReaderAndWriter();
+        rw.init(new SeqClassifierFlags());
+        int numDocs = 0;
+        int numTokens = 0;
+        int numEntities = 0;
+        String lastAnsBase = "";
+        Counter<String> miscCounter = new ClassicCounter<>();
+        StringBuilder inProgressMisc = new StringBuilder();
+        for (Iterator<List<CoreLabel>> it = rw.getIterator(IOUtils.readerFromString(args[0])); it.hasNext();) {
+            List<CoreLabel> doc = it.next();
+            numDocs++;
+            for (CoreLabel fl : doc) {
+                String word = fl.word();
+                // System.out.println("FL " + (++i) + " was " + fl);
+                if (word.equals(BOUNDARY)) {
+                    continue;
+                }
+                String ans = fl.get(CoreAnnotations.AnswerAnnotation.class);
+                String ansBase;
+                String ansPrefix;
+                String[] bits = ans.split("-");
+                if (bits.length == 1) {
+                    ansBase = bits[0];
+                    ansPrefix = "";
+                } else {
+                    ansBase = bits[1];
+                    ansPrefix = bits[0];
+                }
+                numTokens++;
+                if (!ansBase.equals("O")) {
+                    if (ansBase.equals(lastAnsBase)) {
+                        if (ansPrefix.equals("B")) {
+                            numEntities++;
+                            inProgressMisc = maybeIncrementCounter(inProgressMisc, miscCounter);
+                        }
+                    } else {
+                        numEntities++;
+                        inProgressMisc = maybeIncrementCounter(inProgressMisc, miscCounter);
+                    }
+                    if (ansBase.equals("MISC")) {
+                        if (inProgressMisc.length() > 0) {
+                            // already something there
+                            inProgressMisc.append(' ');
+                        }
+                        inProgressMisc.append(word);
+                    }
+                } else {
+                    inProgressMisc = maybeIncrementCounter(inProgressMisc, miscCounter);
+                }
+                lastAnsBase = ansBase;
+            } // for tokens
+        } // for documents
+        System.out.println("File " + args[0] + " has " + numDocs + " documents, " + numTokens + " (non-blank line) tokens and " + numEntities + " entities.");
+        System.out.printf("Here are the %.0f MISC items with counts:%n", miscCounter.totalCount());
+        System.out.println(Counters.toVerticalString(miscCounter, "%.0f\t%s"));
+    } // end main
+
+} // end class CoNLLDocumentReaderAndWriter
---
+++ ner/src/main/java/edu/illinois/cs/cogcomp/ner/StanfordNERTest.java
@@ -0,0 +1,333 @@
+package edu.illinois.cs.cogcomp.ner;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.illinois.cs.cogcomp.lbjava.classify.Classifier;
+import edu.illinois.cs.cogcomp.lbjava.classify.FeatureVector;
+import edu.illinois.cs.cogcomp.lbjava.classify.TestDiscrete;
+import edu.illinois.cs.cogcomp.lbjava.parse.Parser;
+import edu.stanford.nlp.ie.AbstractSequenceClassifier;
+import edu.stanford.nlp.ie.crf.CRFClassifier;
+import edu.stanford.nlp.io.IOUtils;
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreAnnotations.AnswerAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.GoldAnswerAnnotation;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.sequences.SeqClassifierFlags;
+
+/** 
+ * This class tests the provided data, a directory passed as an argument, against the Stanford
+ * NER 4 class model. The input data is expected to be in CoNLL column format, and include the 
+ * 4 classes, PERSON, LOCATION, ORGANIZATION and MISC. It will print two tabular report at the
+ * end using our own TestDiscrete class. The first report will present token level accuracy, the 
+ * second will present phrase level accuracy.
+ */
+public class StanfordNERTest {
+
+    /**
+    * @param args
+    * @throws Exception
+    */
+    public static void main(String[] args) throws Exception {
+        
+        // use the 4 class classifier.
+        String serializedClassifier = "edu/stanford/nlp/models/ner/english.conll.4class.distsim.crf.ser.gz";
+        AbstractSequenceClassifier<CoreLabel> classifier = CRFClassifier.getClassifier(serializedClassifier);
+        File[] files = new File(args[0]).listFiles();
+        List<List<CoreLabel>> result = new ArrayList<List<CoreLabel>>();
+        
+        // apply stanford to each file in the directory. This will result in a data structure that stores
+        // both the gold standard label, AND the prediction, which is handy.
+        for (File file : files) {
+            String fileContents = IOUtils.slurpFile(file.getAbsolutePath());
+            LaterCoNLLReaderAndWriter t = new LaterCoNLLReaderAndWriter(true);
+            SeqClassifierFlags flags = new SeqClassifierFlags();
+            flags.deleteBlankLines = true;
+            t.init(flags);
+            List<List<CoreLabel>> out = classifier.classifyRaw(fileContents, t);
+            
+            // translate LOCATION labels to B-LOC, I-LOC and so on. Same for ORGANIZATION , PEOPLE and MISC
+            for (List<CoreLabel> sentence : out) {
+                CoreLabel previousWord = null;
+                for (CoreLabel word : sentence) {
+                    String currentAnnotation = word.get(CoreAnnotations.AnswerAnnotation.class);
+                    if (!word.get(CoreAnnotations.AnswerAnnotation.class).equals("O")) {
+                        String prevAnnotation = previousWord == null ? "" : previousWord.get(CoreAnnotations.AnswerAnnotation.class);
+                        if (currentAnnotation.equals("LOCATION")) {
+                            if (prevAnnotation.contains("LOC")) {
+                                word.set(CoreAnnotations.AnswerAnnotation.class, "I-LOC");
+                            } else {
+                                word.set(CoreAnnotations.AnswerAnnotation.class, "B-LOC");
+                            }
+                        } else if (currentAnnotation.equals("PERSON")) {
+                            if (prevAnnotation.contains("PER")) {
+                                word.set(CoreAnnotations.AnswerAnnotation.class, "I-PER");
+                            } else {
+                                word.set(CoreAnnotations.AnswerAnnotation.class, "B-PER");
+                            }
+                        } else if (currentAnnotation.equals("ORGANIZATION")) {
+                            if (prevAnnotation.contains("ORG")) {
+                                word.set(CoreAnnotations.AnswerAnnotation.class, "I-ORG");
+                            } else {
+                                word.set(CoreAnnotations.AnswerAnnotation.class, "B-ORG");
+                            }
+                        } else if (currentAnnotation.equals("MISC")) {
+                            if (prevAnnotation.contains("MISC")) {
+                                word.set(CoreAnnotations.AnswerAnnotation.class, "I-MISC");
+                            } else {
+                                word.set(CoreAnnotations.AnswerAnnotation.class, "B-MISC");
+                            }
+                        } else 
+                            System.err.println("WHAT KIND OF ANNOTATION IS "+currentAnnotation);
+                    }
+                    previousWord = word;
+                    //System.out.print(word.word() + '\t' + word.get(CoreAnnotations.AnswerAnnotation.class) + '\t' + word.get(CoreAnnotations.GoldAnswerAnnotation.class) +'\n');
+                }
+            }
+            result.addAll(out);
+        }
+        
+        // produce token level accuracy.
+        System.out.println("Token level accuracy:");
+        TestDiscrete td = TestDiscrete.testDiscrete(new AnswerClassifier(), new GoldClassifier(), new StanfordParser(result, false));
+        td.addNull("O");
+        td.printPerformance(System.out);
+        
+        // produce phrase level accuracy.
+        System.out.println("\nPhrase level accuracy:");
+        td = TestDiscrete.testDiscrete(new AnswerClassifier(), new GoldClassifier(), new StanfordParser(result, true));
+        td.addNull("O");
+        td.printPerformance(System.out);
+
+    }
+}
+
+/**
+ * Produce the gold standard discrete value.
+ * @author redman
+ */
+class GoldClassifier extends Classifier {
+    @Override
+    public FeatureVector classify(Object o) {
+        return null;
+    }
+    
+    /**
+     * Strip off the boundry indicator.
+     * @param label the label to prune
+     * @return the stripped label.
+     */
+    private String prune(String label) {
+        if (label.equals("O"))
+            return label;
+        else
+            return label.substring(2);
+    }
+    /**
+     * return the gold label at this token.
+     * @param o The object to classify.
+     * @return The value of the feature produced for the input object.
+     **/
+    public String discreteValue(Object o) {
+        Class<GoldAnswerAnnotation> cls = CoreAnnotations.GoldAnswerAnnotation.class;
+        if (o instanceof CoreLabel) {
+            // single token.
+            CoreLabel cl = (CoreLabel) o;
+            return cl.get(cls);
+        } else {
+            // compile the phrase hit
+            ArrayList<CoreLabel> hits = (ArrayList)o;
+            String firstLabel = hits.get(0).get(cls);
+            if (hits.size() == 1)
+                return prune(firstLabel);
+            
+            if (!firstLabel.startsWith("B-"))
+                System.err.println("There was a non start tag starting a phrase match in gold!");
+            String continueLabel = "I-"+firstLabel.substring(2);
+            
+            // just make sure the programming logic is correct, we should never see mixed tokens.
+            for (int i = 1; i < hits.size() ; i++) {
+                CoreLabel cl = hits.get(i);
+                if (!cl.get(cls).equals(continueLabel))
+                    System.err.println("There was a non continue tag in a phrase match in gold!");
+            }
+            return prune(firstLabel);
+        }
+    }
+}
+
+/**
+ * produce the prediction
+ * @author redman
+ */
+class AnswerClassifier extends Classifier {
+    /** default */
+    private static final long serialVersionUID = 1L;
+    
+    @Override
+    public FeatureVector classify(Object o) {
+        return null;
+    }
+    /**
+     * return the gold label at this token.
+     * @param o The object to classify.
+     * @return The value of the feature produced for the input object.
+     **/
+    public String discreteValue(Object o) {
+        Class<AnswerAnnotation> cls = CoreAnnotations.AnswerAnnotation.class;
+        if (o instanceof CoreLabel) {
+            // single token.
+            CoreLabel cl = (CoreLabel) o;
+            return cl.get(cls);
+        } else {
+            // compile the phrase hit
+            ArrayList<CoreLabel> hits = (ArrayList)o;
+            return constructLabel(hits);
+        }
+    }
+    
+    /**
+     * Strip off the boundry indicator.
+     * @param label the label to prune
+     * @return the stripped label.
+     */
+    private String prune(String label) {
+        if (label.equals("O"))
+            return label;
+        else
+            return label.substring(2);
+    }
+    
+    /**
+     * Determine what to return. If the input value is "O", and the prediction is
+     * not, return the prediction (less the "B-" or "I-"). If the input is a hit,
+     * and the prediction is a different ,
+     * @param hits the phrase match including all tokens.
+     * @return the list of labels.
+     */
+    private String constructLabel(ArrayList<CoreLabel> hits) {
+        final Class<AnswerAnnotation> cls = CoreAnnotations.AnswerAnnotation.class;
+        final Class<GoldAnswerAnnotation> gcls = CoreAnnotations.GoldAnswerAnnotation.class;
+        String firstLabel = hits.get(0).get(cls);
+        String goldLabel = hits.get(0).get(gcls);
+        
+        // just make sure the programming logic is correct, we should never see mixed tokens.
+        for (int i = 0; i < hits.size() ; i++) {
+            CoreLabel cl = hits.get(i);
+            if (!cl.get(cls).equals(cl.get(gcls))) {
+                
+                // didn't match, decide what to return, make sure it doesn't match.
+                if (goldLabel.equals("O"))
+                    return prune(firstLabel);
+                else {
+                    String pruneGoldLabel = prune(goldLabel);
+                    String pruneAnswerLabel = prune(firstLabel);
+                    if (pruneGoldLabel.equals(pruneAnswerLabel)) {
+                        // well, we can return a correct value here because we are not correct
+                        return "O";
+                    } else {
+                        return pruneAnswerLabel;
+                    }
+                }
+            }
+        }
+        return prune(firstLabel);
+    }
+}
+
+/**
+ * produce the core label set, always only one for token level, but potentially
+ * more if phrase level, and the label spans multiple tokens.
+ * @author redman
+ */
+class StanfordParser implements Parser {
+    
+    /** the labels. */
+    private List<List<CoreLabel>> out = null;
+    
+    /** the index of the current word. */
+    private int sentenceIndx = 0;
+    
+    /** the index of the word within that sentence. */
+    private int wordIndex = 0;
+    
+    /** true if phrase level parsing. */
+    private boolean phrase = false;
+    /**
+     * labels and phrase parsing provided inputs.
+     * @param out the data.
+     * @param phrase if true, parse phrases
+     */
+    StanfordParser(List<List<CoreLabel>> out, boolean phrase) {
+        this.out = out;
+        this.phrase = phrase;
+    }
+    
+    /**
+     * Return a core label for token level, an array of core labels for 
+     * phrase level.
+     */
+    @Override
+    public Object next() {
+        if (phrase) {
+            
+            // we are looking for phrases, so if we see a "B-", we return all tokens in that phrase
+            Class<GoldAnswerAnnotation> goldAnsCls = CoreAnnotations.GoldAnswerAnnotation.class;
+
+            // compile the phrase or a single term.
+            ArrayList<CoreLabel> labels = new ArrayList<CoreLabel>();
+            while (sentenceIndx < out.size()) {
+                if (wordIndex < out.get(sentenceIndx).size()) {
+                    
+                    // we have found the first token, see if label spans more tokens
+                    CoreLabel cl = out.get(sentenceIndx).get(wordIndex++);
+                    labels.add(cl);
+                    if (((String)cl.get(goldAnsCls)).equals("O")) {
+                        
+                        // no annotation on this token.
+                        return labels;
+                    } else {
+                        String continueLabel = "I-"+cl.get(goldAnsCls).substring(2);
+
+                        // look for more tokens in this phrase
+                        while (wordIndex < out.get(sentenceIndx).size()) {
+                            cl = out.get(sentenceIndx).get(wordIndex);
+                            String goldLabel = (String) cl.get(goldAnsCls);
+                            if (!goldLabel.equals(continueLabel)) 
+                                return labels;
+                            else {
+                                labels.add(cl);
+                                wordIndex++;
+                            }
+                        }
+                        return labels; // we are at the end of a sentence.
+                    }
+                } else {
+                    sentenceIndx++;
+                    wordIndex = 0;
+                }
+            }
+        } else {
+            while (sentenceIndx < out.size()) {
+                if (wordIndex < out.get(sentenceIndx).size())
+                    return out.get(sentenceIndx).get(wordIndex++);
+                else {
+                    sentenceIndx++;
+                    wordIndex = 0;
+                }
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void reset() {
+        sentenceIndx = 0;
+        wordIndex = 0;
+    }
+    @Override
+    public void close() {        
+    }
+}
