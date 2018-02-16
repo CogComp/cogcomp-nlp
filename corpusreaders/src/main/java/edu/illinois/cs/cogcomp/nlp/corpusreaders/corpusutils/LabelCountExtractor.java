8608b0422be015363b5f574a194f9c458f072a69
Count Zero Interrupt <marksdsammons@gmail.com>
2018-02-16 09:06:23 -0600

    updating corpus split utility, which is very inefficient (designed for a small corpus with relatively few documents, with the goal of splitting documents and not individual examples).

---
+++ corpusreaders/src/main/java/edu/illinois/cs/cogcomp/nlp/corpusreaders/corpusutils/LabelCountExtractor.java
@@ -0,0 +1,53 @@
+/**
+ * This software is released under the University of Illinois/Research and Academic Use License. See
+ * the LICENSE file in the root folder for details. Copyright (c) 2016
+ *
+ * Developed by: The Cognitive Computation Group University of Illinois at Urbana-Champaign
+ * http://cogcomp.cs.illinois.edu/
+ */
+package edu.illinois.cs.cogcomp.nlp.corpusreaders.corpusutils;
+
+import edu.illinois.cs.cogcomp.core.stats.Counter;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Interface defining API for a class that generates a map of example ID to label (or feature) counts.
+ * Derived classes should define a constructor that seeds the data structures used to generate these counts.
+ *
+ * NOTE: this class is mid-refactor; see TODO
+ *
+ * TODO: change definition of getLabelCounts() to take a generic map of string to example data structure; this will
+ *       obviate the need to store the original data set.
+ * TODO: assess possibility of moving counting logic from derived classes to this class.
+ * @author mssammon
+ */
+public abstract class LabelCountExtractor<K> {
+
+
+    protected Map<String, Counter<String>> labelCounts;
+
+    protected Counter<String> labelTotals;
+
+    public LabelCountExtractor(Map<String, K> examples) {
+        labelCounts = new HashMap<>();
+        labelTotals = new Counter<>();
+
+        populateLabelCounts(examples);
+    }
+
+
+    public Map<String, Counter<String>> getLabelCounts() {
+        return labelCounts;
+    }
+
+    public Counter<String> getLabelTotals() {
+        return labelTotals;
+    }
+
+
+    abstract public void populateLabelCounts(Map<String, K> examples);
+
+    abstract public Map<String, Double> findTargetCounts(double frac);
+}
---
+++ corpusreaders/src/test/java/edu/illinois/cs/cogcomp/nlp/corpusreaders/CreateTrainDevTestSplitTest.java
@@ -0,0 +1,157 @@
+package edu.illinois.cs.cogcomp.nlp.corpusreaders;
+
+import edu.illinois.cs.cogcomp.core.utilities.StringUtils;
+import edu.illinois.cs.cogcomp.nlp.corpusreaders.corpusutils.CreateTrainDevTestSplit;
+import edu.illinois.cs.cogcomp.nlp.corpusreaders.corpusutils.LabelCountExtractor;
+import edu.illinois.cs.cogcomp.nlp.corpusreaders.corpusutils.ListExampleLabelCounter;
+import org.apache.commons.math3.optim.InitialGuess;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * use a synthetic data set to test corpus split creator.
+ *
+ * @author mssammon
+ */
+public class CreateTrainDevTestSplitTest {
+
+    private static final double TRAIN_FRAC = 0.7;
+    private static final double DEV_FRAC = 0.1;
+    private static final double TEST_FRAC = 0.2;
+    private String[] categories;
+    private double[] proportions;
+
+    @Before
+    public void init() {
+
+        categories = new String[] {"Red", "Green", "Yellow", "Blue"};
+        proportions = new double[categories.length];
+        double sum = 0.0;
+
+
+        // set proportions to be 1/(2^n), except make last two proportions equal for convenient fractions
+        for (int i = 0; i < categories.length - 1; ++i) {
+            double prop = 1.0/(double) Math.pow(2, i+1);
+            proportions[i] = prop;
+            sum += prop;
+        }
+        proportions[categories.length - 1] = proportions[categories.length - 2];
+        sum += proportions[categories.length - 1];
+
+        double norm = 1.0 / sum;
+
+        for (int i = 0; i < categories.length; ++i) {
+            proportions[i] *= norm;
+        }
+    }
+
+
+    /**
+     * when data set is small, balancing different characteristics is important.
+     */
+    @Test
+    public void testSmallDataset() {
+
+        Map<String, Map<String, Integer>> smallExampleList = generateListExamples(categories, 128);
+        ListExampleLabelCounter listCounter = new ListExampleLabelCounter(smallExampleList);
+        CreateTrainDevTestSplit createTrainDevTestSplit = new CreateTrainDevTestSplit(listCounter);
+        Map<CreateTrainDevTestSplit.Split, Set<String>> splits =
+                createTrainDevTestSplit.getSplits(TRAIN_FRAC, DEV_FRAC, TEST_FRAC);
+
+        // check that the dev, train and test splits have correct gross sizes
+        assertTrue(Math.abs(128 * TRAIN_FRAC - splits.get(CreateTrainDevTestSplit.Split.TRAIN).size()) < 4);
+        assertTrue(Math.abs(128 * DEV_FRAC - splits.get(CreateTrainDevTestSplit.Split.DEV).size()) < 4);
+        assertTrue(Math.abs(128 * TEST_FRAC - splits.get(CreateTrainDevTestSplit.Split.TEST).size()) < 4);
+
+        double[] featureCounts = getFeatureCounts(splits.get(CreateTrainDevTestSplit.Split.DEV), smallExampleList);
+
+        assertTrue(compareProportions(proportions, featureCounts));
+    }
+
+    private boolean compareProportions(double[] proportions, double[] featureCounts) {
+
+        boolean areProportionsCorrect = true;
+
+        double total = 0;
+        for (int i = 0; i < featureCounts.length; ++i) {
+            total += featureCounts[i];
+        }
+
+        double[] sampleProportions = new double[proportions.length];
+
+        for (int i = 0; i < featureCounts.length; ++i) {
+            sampleProportions[i] = featureCounts[i] / total;
+            if (Math.abs(proportions[i] - sampleProportions[i]) > 0.02) {
+                System.err.println("Split feature " + categories[i] + " has poor proportion: " +
+                "goal is " + String.format("%0.3f", proportions[i]) + "; actual proportion: " +
+                String.format("%0.3f", sampleProportions[i]));
+                areProportionsCorrect = false;
+            }
+        }
+        return areProportionsCorrect;
+    }
+
+    private double[] getFeatureCounts(Set<String> ids, Map<String, Map<String, Integer>> exampleList) {
+
+        Map<String, Integer> rawCounts = new HashMap<>();
+        for (String feat : categories)
+            rawCounts.put(feat, 0);
+
+        for (String id : ids) {
+            Map<String, Integer> ex = exampleList.get(id);
+            for (String feat : ex.keySet())
+                rawCounts.put(feat, rawCounts.get(feat) + ex.get(feat));
+        }
+
+        double[] featureCounts = new double[categories.length]; // allow for dummy feature
+        for (int i = 0; i < categories.length; ++i)
+            featureCounts[i] = (double) rawCounts.get(categories[i]);
+
+        return featureCounts;
+    }
+
+    /**
+     * generate a set of examples with the categories named, with each having a fixed proportion.
+     * numExamples should be a power of 2.
+     *
+     * @param categories
+     * @param numExamples
+     * @return
+     */
+    private Map<String, Map<String, Integer>> generateListExamples(String[] categories, int numExamples) {
+
+        Map<String, Map<String, Integer>> examples = new HashMap<>();
+        int[] numActivePerCat = new int[categories.length];
+        for (int i = 0; i < categories.length; ++i)
+            numActivePerCat[i] = Math.round((float) proportions[i] * numExamples);
+
+        StringBuilder bldr = new StringBuilder();
+        for (int i = 0; i < numActivePerCat.length; ++i) {
+            bldr.append(numActivePerCat[i]).append(",");
+        }
+        System.err.println("## numActive counts: " + bldr.toString());
+
+        for (int i = 0; i < numExamples; ++i) {
+            Map<String, Integer> example = new HashMap<>();
+            example.put("DUMMY", 1); // add this so at least one count is active in every example
+            // to avoid half examples being completely empty
+            if (i < numExamples * proportions[0])
+                example.put(categories[0], 1);
+
+            for (int j = 1; j < proportions.length; ++j)
+                if(0 == i % (numExamples / numActivePerCat[j]))
+                    example.put(categories[j], 1);
+
+            examples.put(String.format("%06d", i), example);
+        }
+
+        return examples;
+    }
+}
